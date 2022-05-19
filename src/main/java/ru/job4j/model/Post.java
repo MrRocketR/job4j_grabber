97951edd1f9c63b1f644304ec9c5233b58ba31f8
MrRocketR <kuptovns@gmail.com>
2022-05-19 20:03:01 +0300

    2.2. Модель данных - Post

--- src/main/java/ru/job4j/model/Post.java
+++ src/main/java/ru/job4j/model/Post.java
@@ -5,11 +5,11 @@ import java.util.Objects;
 
 public class Post {
 
-    int id;
-    String title;
-    String link;
-    String description;
-    LocalDateTime created;
+    private int id;
+    private String title;
+    private String link;
+    private String description;
+    private LocalDateTime created;
 
     public Post(int id, String title, String link, String description, LocalDateTime created) {
         this.id = id;
@@ -59,7 +59,6 @@ public class Post {
         this.created = created;
     }
 
-
     @Override
     public String toString() {
         return "Post{" + "id="
@@ -72,10 +71,15 @@ public class Post {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Post post = (Post) o;
-        return id == post.id && Objects.equals(title, post.title) && Objects.equals(link, post.link);
+        return id == post.id && Objects.equals(title, post.title)
+                && Objects.equals(link, post.link);
     }
 
     @Override
