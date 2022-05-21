a16be69bad34f9a4587389abddf501c356ad1373
MrRocketR <kuptovns@gmail.com>
2022-05-21 20:41:41 +0300

    1.1. Job c параметрами

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -1,10 +1,15 @@
 package ru.job4j.quartz;
 
+
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.*;
@@ -13,42 +18,79 @@ import static org.quartz.SimpleScheduleBuilder.*;
 
 public class AlertRabbit {
 
-    public static Integer getInterval() {
-        Integer interval = null;
+    private static JobDataMap data = new JobDataMap();
+    private static Connection connection;
+
+    public static void init() throws IOException, ClassNotFoundException, SQLException {
+
         try (InputStream in = AlertRabbit.class.getClassLoader()
                 .getResourceAsStream("rabbit.properties")) {
             Properties config = new Properties();
             config.load(in);
-            interval = Integer.valueOf(config.getProperty("rabbit.interval"));
-        } catch (IOException e) {
-            e.printStackTrace();
+            String driver = config.getProperty("driver");
+            Class.forName(driver);
+            connection = DriverManager.getConnection(
+                    config.getProperty("url"),
+                    config.getProperty("username"),
+                    config.getProperty("password")
+            );
+            data.put("connection", connection);
+            data.put("rabbit.interval", config.getProperty("rabbit.interval"));
         }
-        return interval;
     }
 
-    public static void main(String[] args) {
-        int interval = AlertRabbit.getInterval();
+    public static void insertIntoDb(JobDataMap data, LocalDateTime localDateTime) {
+        String sql = "INSERT INTO rabbit (created) values (?)";
+        Timestamp timestampSQL = Timestamp.valueOf(localDateTime);
         try {
+        Connection connection = (Connection) data.get("connection");
+        PreparedStatement pS = connection.prepareStatement(sql);
+        pS.setTimestamp(1, timestampSQL);
+        pS.execute();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException,
+            ClassNotFoundException, SQLException {
+        init();
+        try {
+            List<Long> store = new ArrayList<>();
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
-            JobDetail job = newJob(Rabbit.class).build();
+            data.put("store", store);
+            JobDetail job = newJob(Rabbit.class)
+                    .usingJobData(data)
+                    .build();
             SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(interval)
+                    .withIntervalInSeconds(5)
                     .repeatForever();
             Trigger trigger = newTrigger()
                     .startNow()
                     .withSchedule(times)
                     .build();
             scheduler.scheduleJob(job, trigger);
-        } catch (SchedulerException se) {
+            insertIntoDb(data, LocalDateTime.now());
+            Thread.sleep(5000);
+            scheduler.shutdown();
+            System.out.println(store);
+        } catch (Exception se) {
             se.printStackTrace();
         }
     }
 
     public static class Rabbit implements Job {
+
+        public Rabbit() {
+            System.out.println(hashCode());
+        }
+
         @Override
         public void execute(JobExecutionContext context) throws JobExecutionException {
             System.out.println("Rabbit runs here ...");
+            List<Long> store = (List<Long>) context.getJobDetail().getJobDataMap().get("store");
+            store.add(System.currentTimeMillis());
         }
     }
 }
