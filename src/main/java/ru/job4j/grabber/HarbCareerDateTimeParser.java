8d840ef44abd4838795590a0fb728fd614229056
MrRocketR <kuptovns@gmail.com>
2022-05-23 22:58:10 +0300

    2.1. Преобразование даты

--- src/main/java/ru/job4j/grabber/HarbCareerDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/HarbCareerDateTimeParser.java
@@ -1,13 +1,14 @@
 package ru.job4j.grabber;
 
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 import java.util.Map;
 
 public class HarbCareerDateTimeParser implements DateTimeParser {
 
     @Override
     public LocalDateTime parse(String parse) {
-        LocalDateTime dateTime = LocalDateTime.parse(parse);
-        return dateTime;
+        ZonedDateTime zd = ZonedDateTime.parse(parse);
+        return zd.toLocalDateTime();
     }
 }
