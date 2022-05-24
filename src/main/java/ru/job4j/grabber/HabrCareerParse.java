f9c4c52b288b349171e9491283009e260187dd19
MrRocketR <kuptovns@gmail.com>
2022-05-24 22:34:44 +0300

    2.4. SqlRuParse

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -5,32 +5,38 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
+import ru.job4j.model.Post;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
-public class HabrCareerParse {
+public class HabrCareerParse implements Parse {
 
-    private static final String SOURCE_LINK = "https://career.habr.com";
+    private final DateTimeParser dateTimeParser;
 
-    private static final String PAGE_LINK = String.format("%s/vacancies/java_developer?page=",
-            SOURCE_LINK);
+    public HabrCareerParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
 
-    private String retrieveDescription(String link) throws IOException {
-        String rsl;
-        Connection connection = Jsoup.connect(link);
-        Document document = connection.get();
-        Element descriptionElement = document.selectFirst(".style-ugc");
-        rsl = descriptionElement.text();
+    private String retrieveDescription(String link) {
+        String rsl = null;
+        try {
+            Connection connection = Jsoup.connect(link);
+            Document document = connection.get();
+            Element descriptionElement = document.selectFirst(".style-ugc");
+            rsl = descriptionElement.text();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return rsl;
     }
 
-    public static void main(String[] args) throws IOException {
-        for (int page = 1; page <= 5; page++) {
-            String currentLink = String.format("%s%d", PAGE_LINK, page);
-            Connection connection = Jsoup.connect(currentLink);
-            System.out.println("Parsing now");
-            System.out.println(currentLink);
+    @Override
+    public List<Post> list(String link) {
+        List<Post> listOfPosts = new ArrayList<>();
+        HarbCareerDateTimeParser parser = new HarbCareerDateTimeParser();
+        Connection connection = Jsoup.connect(link);
+        try {
             Document document = connection.get();
             Elements rows = document.select(".vacancy-card__inner");
             rows.forEach(row -> {
@@ -38,11 +44,32 @@ public class HabrCareerParse {
                 Element linkElement = titleElement.child(0);
                 Element dateElement = row.select(".vacancy-card__date").first();
                 Element timeElement = dateElement.select("time").first();
-                String dateTimeOfFirstArticle = timeElement.attr("datetime");
+                String vacancyDate = timeElement.attr("datetime");
                 String vacancyName = titleElement.text();
-                String link = String.format("%s%s",  SOURCE_LINK, linkElement.attr("href"));
-                System.out.printf("%s %s %s %n", vacancyName, dateTimeOfFirstArticle, link);
+                String vacancyLink = linkElement.attr("href");
+                String vacancyDescription = retrieveDescription(vacancyLink);
+                listOfPosts.add(new Post(vacancyName, vacancyLink,
+                        vacancyDescription, parser.parse(vacancyDate)
+                ));
             });
+        } catch (Exception se) {
+            se.printStackTrace();
         }
+        return listOfPosts;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
